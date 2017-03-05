e9fd02967b3b1be701978a0f6211772e922f37a3
M. Tim Jones <mtj@mtjones.com>
2017-03-05 12:15:27 -0700

    Commit the systems file.

---
+++ systems.c
@@ -0,0 +1,44 @@
+// Systems for the CES.
+
+#include "7drltypes.h"
+
+void SystemMovementFunction( )
+{
+   unsigned int entity;
+   unsigned int mask = COMPONENT_MOVEMENT | COMPONENT_ANIMAL;
+   int dy, dx;
+
+   extern World world;
+
+   // Animate the animals first.
+   for ( entity = 0 ; entity < MAX_ENTITIES ; entity++ )
+   {
+      if ( ( world.mask[ entity ] & mask ) == mask )
+      {
+         // If state matches speed, time to move.
+         if ( ++world.movement[ entity ].State >= world.movement[ entity ].Speed )
+         {
+            // Reset the speed state.
+            world.movement[ entity ].State = 0;
+
+            // Animals move randomly.
+            dy = ( getRand( 3 ) - 1 );
+            dx = ( getRand( 3 ) - 1 );
+
+            if ( map_get_item( ( world.location[ entity ].Y + dy ),
+                               ( world.location[ entity ].X + dx ) ) == SPACE_ICON )
+            {
+               map_move_item( world.location[ entity ].Y,
+                              world.location[ entity ].X, dy, dx );
+
+               world.location[ entity ].Y += dy;
+               world.location[ entity ].X += dx;
+            }
+         }
+      }
+
+   }
+
+   return;
+}
+
